d4182e47d977e6ae046b27a9a1f8dbf2168c44a2
János Euler <janos.euler@gmail.com>
2024-11-09 13:23:26 +0100

    Navbar-Footer, Grobes Layout (#31)

--- src/frontend/hosting/src/routes/+page.svelte
+++ src/frontend/hosting/src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
-  import { Alert } from 'flowbite-svelte';
+  import { Alert } from "flowbite-svelte";
 </script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
 
 <div class="p-6">
   <Alert>
