2501f5549e3255862eda91bf907425d16abe8b9c
Leon Lux <leonlux@icloud.com>
2024-11-09 18:03:09 +0100

    add sql to create schema and servers table

---
+++ src/provisioning/migrations/1_initial_schema.up.sql
@@ -0,0 +1,27 @@
+-- Create a new Schema for the service
+CREATE SCHEMA IF NOT EXISTS mch_provisioner;
+
+-- Type that represents the possible states a server can have
+CREATE TYPE server_status AS ENUM ('running', 'stopped', 'restarting');
+
+-- Server table representing gameservers
+CREATE TABLE mch_provisioner.servers(
+    id SERIAL PRIMARY KEY,         -- id of the server
+    addr INET,                     -- IP-Address of the server
+    status server_status NOT NULL, -- Current Server Status
+    port INTEGER NOT NULL,         -- Port the Server is listening on
+    memory_mb INTEGER NOT NULL,    -- Amount of RAM the Server has
+    game VARCHAR(128),             -- Which game this server is for
+    game_version VARCHAR(128),     -- Which game version is running
+    players_max INTEGER            -- How many Players are allowed
+);
+
+-- Table storing server backup information
+CREATE TABLE mch_provisioner.world_backups(
+    id SERIAL PRIMARY KEY,                                              -- Id of the backup
+    server_id SERIAL NOT NULL REFERENCES mch_provisioner.servers,       -- Server the backup belongs to
+    world VARCHAR(128),                                                 -- Name of the backed up world
+    game VARCHAR(128),                                                  -- Name of the game
+    timestamp TIMESTAMP,                                                -- Timestamp of creation
+    size INTEGER                                                        -- Size of the backup in bytes
+);
