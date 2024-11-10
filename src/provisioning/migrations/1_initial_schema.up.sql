1e671d77cd379efc0c938424af3b9f1a388815e8
Leon Lux <leonlux@icloud.com>
2024-11-10 12:23:16 +0100

    update initial schema with gamemode and whitelist

--- src/provisioning/migrations/1_initial_schema.up.sql
+++ src/provisioning/migrations/1_initial_schema.up.sql
@@ -4,6 +4,12 @@ CREATE SCHEMA IF NOT EXISTS mch_provisioner;
 -- Type that represents the possible states a server can have
 CREATE TYPE server_status AS ENUM ('running', 'stopped', 'restarting');
 
+-- Type representing the minecraft game difficulty
+CREATE TYPE difficulty AS ENUM ('peaceful', 'easy', 'normal', 'hard');
+
+-- Type representing the minecraft game mode
+CREATE TYPE game_mode AS ENUM ('creative', 'adventure', 'survival', 'hardcore');
+
 -- Server table representing gameservers
 CREATE TABLE mch_provisioner.servers(
     id SERIAL PRIMARY KEY,         -- id of the server
@@ -13,13 +19,17 @@ CREATE TABLE mch_provisioner.servers(
     memory_mb INTEGER NOT NULL,    -- Amount of RAM the Server has
     game VARCHAR(128),             -- Which game this server is for
     game_version VARCHAR(128),     -- Which game version is running
+    game_mode game_mode,        -- Which game mod is currently active
+    difficulty difficulty,       -- Game difficulty
+    whitelist_enabled BOOLEAN,     -- Whether the whitelist is enabled
     players_max INTEGER            -- How many Players are allowed
 );
 
 -- Table storing server backup information
 CREATE TABLE mch_provisioner.world_backups(
     id SERIAL PRIMARY KEY,                                              -- Id of the backup
-    server_id SERIAL NOT NULL REFERENCES mch_provisioner.servers,       -- Server the backup belongs to
+    server_id SERIAL NOT NULL REFERENCES mch_provisioner.servers
+        ON DELETE CASCADE,                                              -- Server the backup belongs to
     world VARCHAR(128),                                                 -- Name of the backed up world
     game VARCHAR(128),                                                  -- Name of the game
     timestamp TIMESTAMP,                                                -- Timestamp of creation
