22b58ec30fdb37190cf880ae7a3b96966ac0f051
Leon Lux <leonlux@icloud.com>
2024-11-12 14:30:08 +0100

    update config file name

--- src/provisioning/cmd/provisioner/main.go
+++ src/provisioning/cmd/provisioner/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	cfg := config.LoadConfig("config.json")
+	cfg := config.LoadConfig(".env")
 	s, err := sqlx.Open("pgx", cfg.Db.ConnectionURI())
 	if err != nil {
 		panic(err)
