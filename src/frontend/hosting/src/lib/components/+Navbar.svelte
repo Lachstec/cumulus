798c90a91176921d3219118b904fad4b1fab6e66
János Euler <janos.euler@gmail.com>
2024-11-14 14:18:20 +0100

    prettier

--- src/frontend/hosting/src/lib/components/+Navbar.svelte
+++ src/frontend/hosting/src/lib/components/+Navbar.svelte
@@ -16,10 +16,7 @@
 <div class="relative px-8">
   <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
     <NavBrand href="/">
-      <img
-        src="/images/logo.png"
-        class="me-3 h-6 sm:h-9"
-        alt="Hosting Logo" />
+      <img src="/images/logo.png" class="me-3 h-6 sm:h-9" alt="Hosting Logo" />
       <span
         class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
         >MC-Hosting</span>
