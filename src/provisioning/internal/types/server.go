0125e3f6126d0e7a962cece0a56408241addd80d
Leon Lux <leonlux@icloud.com>
2024-11-20 15:50:29 +0100

    add tags to Server struct

--- src/provisioning/internal/types/server.go
+++ src/provisioning/internal/types/server.go
@@ -5,40 +5,40 @@ import "net"
 type ServerStatus string
 
 const (
-	running    ServerStatus = "running"
-	stopped    ServerStatus = "stopped"
-	restarting ServerStatus = "restarting"
+	Running    ServerStatus = "running"
+	Stopped    ServerStatus = "stopped"
+	Restarting ServerStatus = "restarting"
 )
 
 type GameMode string
 
 const (
-	creative  GameMode = "creative"
-	survival  GameMode = "survival"
-	adventure GameMode = "peaceful"
-	hardcore  GameMode = "hardcore"
+	Creative  GameMode = "creative"
+	Survival  GameMode = "survival"
+	Adventure GameMode = "peaceful"
+	Hardcore  GameMode = "hardcore"
 )
 
 type Difficulty string
 
 const (
-	peaceful Difficulty = "peaceful"
-	easy     Difficulty = "easy"
-	normal   Difficulty = "normal"
-	hard     Difficulty = "hard"
+	Peaceful Difficulty = "peaceful"
+	Easy     Difficulty = "easy"
+	Normal   Difficulty = "normal"
+	Hard     Difficulty = "hard"
 )
 
 type Server struct {
 	Id               int64
 	Name             string
-	Address          net.IPNet
+	Address          net.IP `db:"addr"`
 	Status           ServerStatus
 	Port             int
-	Memory           int
+	Memory           int `db:"memory_mb"`
 	Game             string
-	GameVersion      string
-	GameMode         GameMode
+	GameVersion      string   `db:"game_version"`
+	GameMode         GameMode `db:"game_mode"`
 	Difficulty       Difficulty
-	WhitelistEnabled bool
-	PlayersMax       int
+	WhitelistEnabled bool `db:"whitelist_enabled"`
+	PlayersMax       int  `db:"players_max"`
 }
