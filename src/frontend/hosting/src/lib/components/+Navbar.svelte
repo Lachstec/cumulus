5feafc7ee45e5e2465e47e45c215fa8db08cdc78
János Euler <janos.euler@gmail.com>
2024-12-04 17:56:26 +0100

    Auth/auth0 integration (#40)
    * removed unused adapter
    
    * working login/logout - missing ui integration
    
    * move auth to /lib
    
    * more moving
    
    * all files moved to /lib
    
    * Added Log In/Out to Navbar
    
    * added config example
    
    * prettier
    
    * TODO: Cookies, but UI working
    
    * updated routes
    
    * finally understood auth (i think) and handler for not implemented routs
    
    * prettier fixed

--- src/frontend/hosting/src/lib/components/+Navbar.svelte
+++ src/frontend/hosting/src/lib/components/+Navbar.svelte
@@ -1,19 +1,9 @@
 <script>
-  import {
-    Navbar,
-    NavBrand,
-    NavLi,
-    NavUl,
-    NavHamburger,
-    Avatar,
-    Dropdown,
-    DropdownItem,
-    DropdownHeader,
-    DropdownDivider,
-  } from "flowbite-svelte";
+  import { Navbar, NavBrand, NavLi, NavUl } from "flowbite-svelte";
+  import Useritem from "./+Useritem.svelte";
 </script>
 
-<div class="relative px-8">
+<div class="relative px-8 mb-16">
   <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
     <NavBrand href="/">
       <img src="/images/logo.png" class="me-3 h-6 sm:h-9" alt="Hosting Logo" />
@@ -21,27 +11,12 @@
         class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
         >MC-Hosting</span>
     </NavBrand>
-    <div class="flex items-center md:order-2">
-      <Avatar id="avatar-menu" src="/images/example-profile.png" />
-      <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
-    </div>
-    <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-      <DropdownHeader>
-        <span class="block text-sm">Beep Boop</span>
-        <span class="block truncate text-sm font-medium">name@example.com</span>
-      </DropdownHeader>
-      <DropdownItem>Dashboard</DropdownItem>
-      <DropdownItem>Settings</DropdownItem>
-      <DropdownItem href="/user/1/servers">My Servers</DropdownItem>
-      <DropdownDivider />
-      <DropdownItem>Sign out</DropdownItem>
-    </Dropdown>
     <NavUl>
       <NavLi href="/" active={true}>Home</NavLi>
-      <NavLi href="/servers">Server</NavLi>
-      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/user/1/servers">Server</NavLi>
       <NavLi href="/pricing">Pricing</NavLi>
       <NavLi href="/contact">Contact</NavLi>
     </NavUl>
+    <Useritem></Useritem>
   </Navbar>
 </div>
---
+++ src/frontend/hosting/src/lib/components/+Useritem.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import {
+    Avatar,
+    DropdownDivider,
+    DropdownHeader,
+    DropdownItem,
+    NavHamburger,
+    Dropdown,
+  } from "flowbite-svelte";
+
+  // @ts-nocheck
+  import { onMount } from "svelte";
+  import auth from "$lib/service/auth_service";
+  import { isAuthenticated, user } from "$lib/store/auth_store";
+  import type { Auth0Client } from "@auth0/auth0-spa-js";
+
+  let auth0Client: Auth0Client;
+
+  onMount(async () => {
+    console.log("onMountCalled");
+
+    auth0Client = await auth.createClient();
+
+    isAuthenticated.set(await auth0Client.checkSession());
+    user.set(await auth0Client.getUser());
+  });
+
+  async function login() {
+    await auth.loginPopup(auth0Client);
+    isAuthenticated.set(await auth0Client.isAuthenticated()); // Update authentication status
+    user.set(await auth0Client.getUser()); // Update user data
+  }
+
+  async function logout() {
+    await auth.logout(auth0Client);
+    isAuthenticated.set(false); // Reset authentication status
+    user.set(null); // Reset user data
+  }
+
+  async function getToken() {
+    try {
+      const token = await auth0Client.getTokenSilently();
+      console.log("Token:", token);
+    } catch (error) {
+      console.error("Error getting token:", error);
+    }
+  }
+
+  const authStatus = $isAuthenticated;
+  const userData = $user;
+</script>
+
+<button on:click={getToken}>Get Token</button>
+{#if $isAuthenticated}
+  <div class="flex items-center space-x-4" id="user-menu">
+    <Avatar src={$user?.picture} />
+    <div class="space-y-1 font-medium dark:text-white">{$user?.nickname}</div>
+    <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+  </div>
+  <Dropdown placement="bottom" triggeredBy="#user-menu">
+    <DropdownHeader>
+      <span class="block text-sm">{$user?.name}</span>
+      <span class="block truncate text-sm font-medium">{$user?.email}</span>
+    </DropdownHeader>
+    <DropdownItem>Dashboard</DropdownItem>
+    <DropdownItem href="/user/1">Settings</DropdownItem>
+    <DropdownItem href="/user/1/servers">My Servers</DropdownItem>
+    <DropdownDivider />
+    <DropdownItem
+      ><a class="nav-link" href="/#" on:click={logout}>Sign Out</a
+      ></DropdownItem>
+  </Dropdown>
+{:else}
+  <div class="flex items-center space-x-4">
+    <Avatar />
+    <div class="space-y-1 font-medium dark:text-white">
+      <a class="nav-link" href="/#" on:click={login}>Sign In</a>
+    </div>
+    <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+  </div>
+{/if}
