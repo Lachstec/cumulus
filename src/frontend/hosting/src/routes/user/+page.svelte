fa646c9774d9ecd8ad53acde6af75a4820f9d0c2
János Euler <janos.euler@gmail.com>
2024-12-05 19:55:17 +0100

    overview with all users, fetching

---
+++ src/frontend/hosting/src/routes/user/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+    import {Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell} from "flowbite-svelte";
+
+    type UserData = {
+        ID: number,
+        Sub: "",
+        name: string,
+        role: string,
+    }
+
+    let data: UserData[]
+    onMount(async () => {
+        const res = await fetch("http://localhost:10000/users");
+        data = await res.json();
+        console.log(data);
+    });
+
+    function deleteUser(ID) {
+        return fetch(`http://localhost:10000/users/${ID}`, {
+            method: "DELETE",
+        })
+    }
+</script>
+
+<ul>
+    <div class="p-8 mt-16 bg-white dark:bg-gray-900 h-screen">
+        <Table>
+            <caption
+                    class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                Users
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+                    View, Edit and Delete Users
+                </p>
+            </caption>
+            <TableHead>
+                <TableHeadCell>Username</TableHeadCell>
+                <TableHeadCell>Role</TableHeadCell>
+                <TableHeadCell>Options</TableHeadCell>
+            </TableHead>
+            <TableBody tableBodyClass="divide-y">
+                {#each data as { name, role, ID }}
+                    <TableBodyRow>
+                        <TableBodyCell>{name}</TableBodyCell>
+                        <TableBodyCell>{role}</TableBodyCell>
+                        <TableBodyCell>
+                            <a
+                                href="../../user/{ID}"
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            >View</a> | Edit |
+                            <a
+                                href="#"
+                                on:click={() => {
+                                        confirm("Do you really want to delete this user?")
+                                        deleteUser(ID)
+                                        console.log("Fetus Deletus")
+                                    }}
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            >Delete</a>
+                        </TableBodyCell>
+                    </TableBodyRow>
+                {/each}
+            </TableBody>
+        </Table>
+    </div>
+</ul>
