407c937c25857cc667b6d92c6693a4ea74e9d48f
Leon Lux <leonlux@icloud.com>
2024-12-06 11:12:36 +0100

    add client wrapping openstack provider

---
+++ src/provisioning/internal/openstack/openstack_client.go
@@ -0,0 +1,35 @@
+package openstack
+
+import (
+	"context"
+	"github.com/Lachstec/mc-hosting/internal/config"
+	"github.com/gophercloud/gophercloud/v2"
+	"github.com/gophercloud/gophercloud/v2/openstack"
+)
+
+// Client is a factory struct that can construct Clients
+// for the various OpenStack Services. The Clients returned from it all
+// use the configuration that was used to create the Client by calling NewClient.
+type Client struct {
+	options gophercloud.AuthOptions
+	client  *gophercloud.ProviderClient
+}
+
+// NewClient creates a new Client that uses the
+// account specified in config to connect to OpenStack.
+func NewClient(config config.Config) (*Client, error) {
+	authOpts := config.Openstack.AuthOptions()
+	ctx := context.Background()
+	provider, err := openstack.AuthenticatedClient(ctx, authOpts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{
+		options: config.Openstack.AuthOptions(),
+		client:  provider,
+	}
+
+	return client, nil
+}
