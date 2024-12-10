cb3778ba720d01efd91c40241ceb02f99360bfed
Leon Lux <leonlux@icloud.com>
2024-12-10 14:05:31 +0100

    update config tests

--- src/provisioning/internal/config/config_test.go
+++ src/provisioning/internal/config/config_test.go
@@ -13,6 +13,7 @@ func TestLoadConfig(t *testing.T) {
 	os.Setenv("DB_USER", "sample_user")
 	os.Setenv("DB_PASS", "sample_pass")
 	os.Setenv("AUTH0_URL", "https://auth0.com/test")
+	os.Setenv("AUTH0_JWKS_URL", "https://auth0.com/jwks")
 
 	defer func() {
 		os.Unsetenv("DB_HOST")
@@ -20,6 +21,7 @@ func TestLoadConfig(t *testing.T) {
 		os.Unsetenv("DB_USER")
 		os.Unsetenv("DB_PASS")
 		os.Unsetenv("AUTH0_URL")
+		os.Unsetenv("AUTH0_JWKS_URL")
 	}()
 
 	cfg := LoadConfig()
@@ -28,7 +30,8 @@ func TestLoadConfig(t *testing.T) {
 	assert.Equal(t, "1337", cfg.Db.Port)
 	assert.Equal(t, "sample_user", cfg.Db.User)
 	assert.Equal(t, "sample_pass", cfg.Db.Password)
-	assert.Equal(t, url.URL{Scheme: "https", Host: "auth0.com", Path: "/test"}, cfg.Auth0.Url)
+	assert.Equal(t, url.URL{Scheme: "https", Host: "auth0.com", Path: "/test"}, cfg.Auth0.AuthURL)
+	assert.Equal(t, url.URL{Scheme: "https", Host: "auth0.com", Path: "/jwks"}, cfg.Auth0.JWKSURL)
 }
 
 func TestFallbackValues(t *testing.T) {
@@ -38,5 +41,6 @@ func TestFallbackValues(t *testing.T) {
 	assert.Equal(t, "5432", cfg.Db.Port)
 	assert.Equal(t, "postgres", cfg.Db.User)
 	assert.Equal(t, "postgres", cfg.Db.Password)
-	assert.Equal(t, url.URL{Scheme: "http", Host: "localhost"}, cfg.Auth0.Url)
+	assert.Equal(t, url.URL{Scheme: "http", Host: "localhost"}, cfg.Auth0.AuthURL)
+	assert.Equal(t, url.URL{Scheme: "http", Host: "localhost"}, cfg.Auth0.JWKSURL)
 }
