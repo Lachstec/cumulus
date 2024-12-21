42a0a65b5b61dc7e14ecda5059606e558f58c157
János Euler <janos.euler@gmail.com>
2024-12-21 18:01:04 +0100

    switched auth_config.js to use .env variables

---
+++ src/frontend/hosting/src/lib/config/auth_config.js
@@ -0,0 +1,14 @@
+import {
+  PUBLIC_AUTH_DOMAIN,
+  PUBLIC_AUTH_CLIENT_ID,
+  PUBLIC_AUTH_AUDIENCE,
+  PUBLIC_AUTH_CACHE_LOCATION
+} from '$env/static/public';
+
+const config = {
+  domain: PUBLIC_AUTH_DOMAIN,
+  clientId: PUBLIC_AUTH_CLIENT_ID,
+  audience: PUBLIC_AUTH_AUDIENCE, // Optional but needed for access tokens
+  cacheLocation: PUBLIC_AUTH_CACHE_LOCATION, // Optional: Use localStorage to persist tokens
+};
+export default config;
