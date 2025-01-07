fa2b132ee7f21d235cd6b9910a81e35d141cd2b8
Leon Lux <leonlux@icloud.com>
2025-01-07 18:36:46 +0100

    change tracing setup to use grpc

--- src/provisioning/internal/tracing/provider.go
+++ src/provisioning/internal/tracing/provider.go
@@ -2,7 +2,6 @@ package tracing
 
 import (
 	"context"
-	"errors"
 	"github.com/Lachstec/mc-hosting/internal/config"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
@@ -10,81 +9,27 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
-
-func SetupTracingSDK(ctx context.Context, config config.TracingConfig) (shutdown func(context.Context) error, err error) {
-	var shutdownFuncs []func(context.Context) error
-
-	shutdown = func(ctx context.Context) error {
-		var err error
-		for _, fn := range shutdownFuncs {
-			err = errors.Join(err, fn(ctx))
-		}
-		shutdownFuncs = nil
-		return err
-	}
-
-	handleErr := func(inErr error) {
-		err = errors.Join(inErr, shutdown(ctx))
-	}
-
-	propagator := newPropagator()
-	otel.SetTextMapPropagator(propagator)
-
-	conn, err := initGRPCConnection(config)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, func(ctx context.Context) error {
-		return conn.Close()
-	})
-
-	res, err := resource.New(ctx,
-		resource.WithAttributes(
-			semconv.ServiceNameKey.String(config.ServiceName),
-		),
-	)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-
-	provider, err := newTraceProvider(ctx, res, conn)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, provider)
-
-	return
-}
 
 func newPropagator() propagation.TextMapPropagator {
 	return propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
 }
 
-func initGRPCConnection(config config.TracingConfig) (*grpc.ClientConn, error) {
-	if config.InsecureConnection {
-		conn, err := grpc.NewClient(config.Endpoint,
-			grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			return nil, err
-		}
-		return conn, nil
-	} else {
-		// TODO: Implement secure connection for production
-		panic("not implemented")
-	}
+func resources(config config.TracingConfig) *resource.Resource {
+	res := resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String(config.ServiceName))
+	return res
 }
 
-func newTraceProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
-	exporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+func NewTraceProvider(ctx context.Context, config config.TracingConfig) (*sdktrace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(
+		ctx,
+		otlptracegrpc.WithEndpoint(config.Endpoint),
+		otlptracegrpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
+
+	res := resources(config)
 
 	batchspan := sdktrace.NewBatchSpanProcessor(exporter)
 	provider := sdktrace.NewTracerProvider(
@@ -94,5 +39,8 @@ func newTraceProvider(ctx context.Context, res *resource.Resource, conn *grpc.Cl
 	)
 	otel.SetTracerProvider(provider)
 
-	return provider.Shutdown, nil
+	propagator := newPropagator()
+	otel.SetTextMapPropagator(propagator)
+
+	return provider, nil
 }
