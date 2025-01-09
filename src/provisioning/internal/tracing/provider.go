351fad37835a27af7bf22369ad9c9b61cb046fb5
Leon Lux <leonlux@icloud.com>
2025-01-09 19:07:36 +0100

    refactor tracing sdk (again :()

--- src/provisioning/internal/tracing/provider.go
+++ src/provisioning/internal/tracing/provider.go
@@ -2,45 +2,52 @@ package tracing
 
 import (
 	"context"
-	"github.com/Lachstec/mc-hosting/internal/config"
+	"errors"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"time"
 )
 
-func newPropagator() propagation.TextMapPropagator {
-	return propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+// SetupOTEL initializes an OpenTelemetry Tracing Provider and registers it with the Telemtry SDK.
+// The created provider pretty-prints to stdout by default.
+func SetupOTEL(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	shutdown = func(ctx context.Context) error {
+		var err error
+
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+
+		shutdownFuncs = nil
+		return err
+	}
+
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	tracer, err := newTracer(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	shutdownFuncs = append(shutdownFuncs, tracer.Shutdown)
+	otel.SetTracerProvider(tracer)
+
+	return
 }
 
-func resources(config config.TracingConfig) *resource.Resource {
-	res := resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String(config.ServiceName))
-	return res
-}
-
-func NewTraceProvider(ctx context.Context, config config.TracingConfig) (*sdktrace.TracerProvider, error) {
-	exporter, err := otlptracegrpc.New(
-		ctx,
-		otlptracegrpc.WithEndpoint(config.Endpoint),
-		otlptracegrpc.WithInsecure())
+func newTracer(ctx context.Context) (*trace.TracerProvider, error) {
+	exporter, err := stdouttrace.New(
+		stdouttrace.WithPrettyPrint())
 	if err != nil {
 		return nil, err
 	}
 
-	res := resources(config)
-
-	batchspan := sdktrace.NewBatchSpanProcessor(exporter)
-	provider := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithResource(res),
-		sdktrace.WithSpanProcessor(batchspan),
-	)
-	otel.SetTracerProvider(provider)
-
-	propagator := newPropagator()
-	otel.SetTextMapPropagator(propagator)
-
+	provider := trace.NewTracerProvider(trace.WithBatcher(exporter, trace.WithBatchTimeout(time.Second)))
 	return provider, nil
 }
