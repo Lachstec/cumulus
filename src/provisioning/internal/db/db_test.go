dca3b29c45110fded3f96069636519e0d670c53d
Leon Lux <leonlux@icloud.com>
2025-01-14 19:12:37 +0100

    update tests to reflect new schema

--- src/provisioning/internal/db/db_test.go
+++ src/provisioning/internal/db/db_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Lachstec/mc-hosting/internal/types"
 	"github.com/google/go-cmp/cmp"
 	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
 	"time"
@@ -38,6 +39,18 @@ func TestServerStore(t *testing.T) {
 		Name:  "Testuser",
 		Class: string(types.Admin),
 	}
+	keyStore := NewKeyStore(db.Db)
+
+	key := types.Key{
+		Name:       "key1",
+		PrivateKey: []byte{1, 2, 3},
+		PublicKey:  []byte{4, 5, 6},
+	}
+
+	kid, err := keyStore.Add(&key)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	uid, err := userStore.Add(user)
 	if err != nil {
@@ -46,7 +59,7 @@ func TestServerStore(t *testing.T) {
 
 	server := &types.Server{
 		UserID:           uid,
-		OpenstackID:      "31e0683c-5455-4510-b3ba-3c02241a3eff",
+		OpenstackId:      "31e0683c-5455-4510-b3ba-3c02241a3eff",
 		Name:             "Test Server",
 		Address:          net.ParseIP("192.168.1.1"),
 		Status:           types.Stopped,
@@ -60,7 +73,7 @@ func TestServerStore(t *testing.T) {
 		WhitelistEnabled: false,
 		PvPEnabled:       true,
 		PlayersMax:       2,
-		SSHKey:           []byte("sample ssh key"),
+		SSHKey:           kid,
 	}
 
 	ID, err := serverStore.Add(server)
@@ -131,5 +144,46 @@ func TestServerStore(t *testing.T) {
 	err = serverStore.Delete(find[0])
 	if err != nil {
 		t.Fatalf("unable to delete server: %s", err)
+	}
+}
+
+func TestKeyStore(t *testing.T) {
+	db := NewTestConnection()
+	keyStore := NewKeyStore(db.Db)
+
+	id, err := keyStore.Add(&types.Key{Name: "Test Key", PrivateKey: []byte("test key"), PublicKey: []byte("test pub key")})
+	if err != nil {
+		t.Fatalf("unable to save key: %s", err)
+	}
+
+	key, err := keyStore.GetById(id)
+	if err != nil {
+		t.Fatalf("unable to get key: %s", err)
+	}
+
+	assert.Equal(t, key.Name, "Test Key")
+	assert.Equal(t, key.PrivateKey, []byte("test key"))
+	assert.Equal(t, key.PublicKey, []byte("test pub key"))
+
+	key.Name = "Another Key"
+	updated, err := keyStore.Update(key)
+	if err != nil {
+		t.Fatalf("unable to update key: %s", err)
+	}
+
+	assert.Equal(t, updated.Name, "Another Key")
+
+	err = keyStore.Delete(key)
+	if err != nil {
+		t.Fatalf("unable to delete key: %s", err)
+	}
+
+	keys, err := keyStore.Find(func(k *types.Key) bool { return k.Id == updated.Id })
+	if err != nil {
+		t.Fatalf("unable to find keys: %s", err)
+	}
+
+	if len(keys) != 0 {
+		t.Fatalf("expected 0 keys, got %d", len(keys))
 	}
 }
