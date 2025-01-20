8d1c8dc22dd727d998094337b63b4bead0afdeb9
Leon Lux <leonlux@icloud.com>
2025-01-20 13:17:53 +0100

    change flavour to have an int as id

--- src/provisioning/internal/types/flavours.go
+++ src/provisioning/internal/types/flavours.go
@@ -1,59 +1,59 @@
 package types
 
 type Flavour struct {
-	ID string
-	Name string
-	RAM	int
-	Disk int
+	ID        int64
+	Name      string
+	RAM       int
+	Disk      int
 	Ephemeral int
-	VCPUs int
-	Is_Public bool
+	VCPUs     int
+	IsPublic  bool
 }
 
-var Flavours = []Flavour {
+var Flavours = []Flavour{
 	{
-		ID: "1",
-		Name: "m1.tiny",
-		RAM: 512,
-		Disk: 1,
+		ID:        1,
+		Name:      "m1.tiny",
+		RAM:       512,
+		Disk:      1,
 		Ephemeral: 0,
-		VCPUs: 1,
-		Is_Public: true,
+		VCPUs:     1,
+		IsPublic:  true,
 	},
 	{
-		ID: "2",
-		Name: "m1.small",
-		RAM: 2048,
-		Disk: 20,
+		ID:        2,
+		Name:      "m1.small",
+		RAM:       2048,
+		Disk:      20,
 		Ephemeral: 0,
-		VCPUs: 1,
-		Is_Public: true,
+		VCPUs:     1,
+		IsPublic:  true,
 	},
 	{
-		ID: "3",
-		Name: "m1.medium",
-		RAM: 4096,
-		Disk: 40,
+		ID:        3,
+		Name:      "m1.medium",
+		RAM:       4096,
+		Disk:      40,
 		Ephemeral: 0,
-		VCPUs: 2,
-		Is_Public: true,
+		VCPUs:     2,
+		IsPublic:  true,
 	},
 	{
-		ID: "4",
-		Name: "m1.large",
-		RAM: 8192,
-		Disk: 80,
+		ID:        4,
+		Name:      "m1.large",
+		RAM:       8192,
+		Disk:      80,
 		Ephemeral: 0,
-		VCPUs: 4,
-		Is_Public: true,
+		VCPUs:     4,
+		IsPublic:  true,
 	},
 	{
-		ID: "5",
-		Name: "m1.xlarge",
-		RAM: 16384,
-		Disk: 160,
+		ID:        5,
+		Name:      "m1.xlarge",
+		RAM:       16384,
+		Disk:      160,
 		Ephemeral: 0,
-		VCPUs: 8,
-		Is_Public: true,
+		VCPUs:     8,
+		IsPublic:  true,
 	},
 }
