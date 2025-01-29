0ab256f2aacb9993bd6ce7f9cb8314e7b6525632
János Euler <janos.euler@gmail.com>
2025-01-29 10:06:26 +0100

    systemdn't

--- src/frontend/hosting/src/routes/server/+page.svelte
+++ src/frontend/hosting/src/routes/server/+page.svelte
@@ -31,7 +31,7 @@
         body: JSON.stringify({
           flavour: cards[flavour].ID,
           name: "MyServer" + uuid,
-          image: "29a24dc0-b24b-4cc8-b43b-a8a4c6916d0f",
+          image: "d6d1835c-7180-4ca9-b4a1-470afbd8b398",
           game: "minecraft",
           game_version: "latest",
           gamemode: "survival",
