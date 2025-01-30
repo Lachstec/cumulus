217833b2ffd534cd8808a6ad2a43d7f972e9b976
János Euler <janos.euler@gmail.com>
2025-01-30 11:10:34 +0100

    file +page.ts was weird

--- src/frontend/src/routes/user/[userid]/+page.ts
+++ src/frontend/src/routes/user/[userid]/+page.ts
@@ -5,20 +5,20 @@ import { PUBLIC_BACKEND_URL } from "$env/static/public";
 let backend_url = PUBLIC_BACKEND_URL;
 
 export const load: PageLoad = async ({ params }) => {
-  const url = `${backend_url}/users/${params.userid}`;
+    const url = `${backend_url}/users/${params.userid}`;
 
-  const res = await fetch(url, {
-    method: "GET",
-  });
+    const res = await fetch(url, {
+        method: "GET",
+    });
 
-  if (!res.ok) {
-    throw error(res.status, "Not found");
-  }
+    if (!res.ok) {
+        throw error(res.status, "Not found");
+    }
 
-  const data = await res.json();
+    const data = await res.json();
 
-  return {
-    name: data.name,
-    role: data.role,
-  };
+    return {
+        name: data.name,
+        role: data.role,
+    };
 };
