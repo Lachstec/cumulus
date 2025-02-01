f8048c43974351372231cbaae56e3d6478d3bc0a
János Euler <janos-conrad.euler@informatik.hs-fulda.de>
2025-02-01 12:39:31 +0100

    small fix to prettier

--- src/frontend/src/routes/user/[userid]/servers/+page.ts
+++ src/frontend/src/routes/user/[userid]/servers/+page.ts
@@ -2,12 +2,11 @@ import type { PageLoad } from "./$types";
 import { env } from "$env/dynamic/public";
 import auth from "$lib/service/auth_service";
 
-
 export const load: PageLoad = async ({ fetch }) => {
   const auth0Client = await auth.createClient();
   const token = await auth0Client.getTokenSilently();
 
-  const res = await fetch(`${env.PUBLIC_BACKEND_URL}/users/1/servers`,{
+  const res = await fetch(`${env.PUBLIC_BACKEND_URL}/users/1/servers`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
