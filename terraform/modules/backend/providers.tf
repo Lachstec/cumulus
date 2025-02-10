a2ae435281fd1e567be14e72acbc37f2823f64d3
Leon Lux <leonlux@icloud.com>
2025-02-10 12:59:26 +0100

    add barebone module for backend deployment

---
+++ terraform/modules/backend/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+  }
+}
+
+provider "openstack" {
+  insecure = true
+}
---
+++ terraform/modules/backend/variables.tf
@@ -0,0 +1,102 @@
+variable "backend_image_id" {
+    description = "The Cloud Image to use for the Backend Instance"
+    type = string
+}
+
+variable "backend_flavor_id" {
+    description = "The Flavor ID to use for the Backend Instance"
+    type = string
+}
+
+variable "backend_network_name" {
+    description = "Name of the network for the Backend servers"
+    type = string
+    default = "backend-network"
+}
+
+variable "backend_subnet_name" {
+  description = "Name of the subnet dedicated for Backend services"
+  type = string
+  default = "backend-subnet"
+}
+
+variable "backend_subnet_cidr" {
+  description = "CIDR Notation of backend subnet address and mask"
+  type = string
+  default = "10.10.10.16/28"
+}
+
+variable "backend_security_group_name" {
+    description = "Name of the security group for the backend"
+    type = string
+    default = "backend-sg"
+}
+
+variable "backend_db_host" {
+  description = "Hostname where a PostgreSQL Database is reachable for the backend"
+  type = string
+}
+
+variable "backend_db_port" {
+  description = "Port of the PostgreSQL Database"
+  type = string
+}
+
+variable "backend_db_user" {
+  description = "Username of the Database user to use"
+  type = string
+}
+
+variable "backend_db_password" {
+  description = "Password of the Database user to use"
+  type = string
+  sensitive = true
+}
+
+variable "openstack_auth_url" {
+    description = "The OpenStack authentication URL"
+    type = string
+}
+
+variable "openstack_region" {
+    description = "The OpenStack region to use"
+    type = string
+}
+
+variable "openstack_username" {
+    description = "OpenStack username to use for deployment"
+    type = string
+}
+
+variable "openstack_password" {
+    description = "OpenStack password to use for deployment"
+    type = string
+    sensitive = true
+}
+
+variable "openstack_tenant" {
+    description = "OpenStack tenant or project name"
+    type = string
+}
+
+variable "openstack_domain_name" {
+    description = "OpenStack domain name"
+    type = string
+    default = "Default"
+}
+
+variable "backend_crypto_key" {
+  description = "Key used to encrypt sensitive information in the database. Must be Base64 encoded and of proper length for AES-256"
+  type = string
+  sensitive = true
+}
+
+variable "backend_tracing_endpoint" {
+    description = "Endpoint where Logs can be sent to"
+    type = string
+}
+
+variable "backend_tracing_service_name" {
+    description = "The service name to use when sending logs"
+    type = string
+}
