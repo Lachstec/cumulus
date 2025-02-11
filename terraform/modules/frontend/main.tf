f158a4589fe83026516a6b55a2eac8274b179100
János Euler <janos.euler@gmail.com>
2025-02-11 14:53:34 +0100

    Floating Ip und grobe struktur frontend

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -11,6 +11,16 @@ terraform {
   }
 }
 
+provider "openstack" {
+  auth_url     = "#"
+  region       = "#"
+  password     = "#"
+  domain_name  = "#"
+  user_name     = "#"
+  tenant_name   = "#"
+  insecure = true
+}
+
 # Create a Keypair for the backend instances
 resource "tls_private_key" "generated" {
   algorithm = "RSA"
@@ -71,4 +81,59 @@ resource "openstack_compute_instance_v2" "frontend_servers" {
     PUBLIC_BACKEND_URL          = var.frontend_backend_url
     PUBLIC_REQUESTER_NAME       = var.frontend_requester_name
   })
+} 
+
+resource "openstack_lb_loadbalancer_v2" "frontend_loadbalancer" {
+  name = var.frontend_loadbalancer_name
+  vip_subnet_id = openstack_networking_subnet_v2.frontend_subnet.id
+  description = "Loadbalancer for frontend services"
 }
+
+resource "openstack_lb_listener_v2" "frontend_loadbalancer_listener" {
+  name = format("%s/%s",var.frontend_loadbalancer_name,"_listener")
+  protocol = "TCP"
+  protocol_port = var.frontend_port
+  loadbalancer_id = openstack_lb_loadbalancer_v2.frontend_loadbalancer.id
+}
+
+resource "openstack_lb_pool_v2" "frontend_loadbalancer_pool" {
+  name = format("%s/%s",var.frontend_loadbalancer_name,"_pool")
+    protocol = "TCP"
+    lb_method = "ROUND_ROBIN"
+    listener_id = openstack_lb_listener_v2.frontend_loadbalancer_listener.id
+}
+
+resource "openstack_lb_member_v2" "frontend_loadbalancer_members" {
+  count             = 2
+  pool_id           = openstack_lb_pool_v2.frontend_loadbalancer_pool.id
+  address           = openstack_compute_instance_v2.frontend_servers[count.index].access_ip_v4
+  protocol_port     = var.frontend_port
+  subnet_id         = openstack_networking_subnet_v2.frontend_subnet.id
+}
+
+resource "openstack_lb_monitor_v2" "frontend_loadbalancer_healthcheck" {
+  pool_id = openstack_lb_pool_v2.frontend_loadbalancer_pool.id
+  type = "HTTP"
+  delay             = 5
+  timeout           = 5
+  max_retries       = 3
+}
+
+# Neeed a router for the frontend
+resource "openstack_networking_router_v2" "frontend_router" {
+  name = "frontend-router"
+  external_network_id = "6f530989-999a-49e6-9197-8a33ae7bfce7"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+    router_id = openstack_networking_router_v2.frontend_router.id
+    subnet_id = openstack_networking_subnet_v2.frontend_subnet.id
+}
+
+# Creating floating IP for the frontend - to test. Should happen in Main before Auth0
+resource "openstack_networking_floatingip_v2" "frontend_floatingip" {
+  pool = "ext_net"
+  description = "frontend floating ip"
+  port_id = openstack_lb_loadbalancer_v2.frontend_loadbalancer.vip_port_id
+}
+# Here we could create the Auth0 service, and then create the frontend itself
