d16d9c66aba4dacb72ff5256a9bf15d7535dc88b
Leon Lux <leonlux@icloud.com>
2025-02-11 18:21:29 +0100

    add module to provision two floating ips for the loadbalancers

---
+++ terraform/modules/floating_ips/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "backend_lb_floating_ip" {
+  pool = var.external_network_name
+}
+
+resource "openstack_networking_floatingip_v2" "frontend_lb_floating_ip" {
+  pool = var.external_network_name
+}
---
+++ terraform/modules/floating_ips/variables.tf
@@ -0,0 +1,4 @@
+variable "external_network_name" {
+  description = "ID of the external network to use for floating ips"
+  type = string
+}
