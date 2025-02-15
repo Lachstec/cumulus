d4dee2a7f949530368c22656aa717f219962c558
Leon Lux <leonlux@icloud.com>
2025-02-15 20:54:17 +0100

    use new api structure for users resource

--- src/provisioning/cmd/provisioner/main.go
+++ src/provisioning/cmd/provisioner/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Lachstec/mc-hosting/internal/api"
 	"github.com/Lachstec/mc-hosting/internal/config"
 	"github.com/Lachstec/mc-hosting/internal/db"
 	"github.com/Lachstec/mc-hosting/internal/logging"
@@ -69,132 +70,13 @@ func main() {
 	router.Use(services.CORSMiddleware())
 	router.Use(logging.LoggingMiddleware(cfg.LoggingConfig))
 
-	router.GET("/users", func(c *gin.Context) {
-		users, err := userService.ReadAllUsers()
-		if err != nil {
-			l.Error().Err(err).Msg("failed to fetch users from database")
-			c.String(http.StatusInternalServerError, "failed to fetch users")
-		}
-		c.JSON(http.StatusOK, users)
-	})
+	handler := &api.Handler{
+		UserService:   *userService,
+		ServerService: *serverService,
+		Logger:        &l,
+	}
 
-	router.POST("/users", func(c *gin.Context) {
-		var user *types.User
-		err := c.BindJSON(&user)
-		if err != nil {
-			l.Warn().Err(err).Msg("invalid payload for new user")
-			c.String(http.StatusUnprocessableEntity, "invalid user format")
-			return
-		}
-		userid, err := userService.CreateUser(user)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to save user to database")
-			c.String(http.StatusInternalServerError, "failed to create new user")
-			return
-		}
-		c.JSON(http.StatusOK, userid)
-	})
-
-	router.GET("/users/:userid", func(c *gin.Context) {
-		userid, err := urlParamToInt64(c.Param("userid"))
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to extract user id from request")
-			c.String(http.StatusBadRequest, "expected user id in url param")
-			return
-		}
-		users, err := userService.ReadUserByUserID(userid)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to retrieve user from database")
-			c.String(http.StatusInternalServerError, "failed to retrieve user")
-			return
-		}
-		if len(users) == 0 {
-			c.String(http.StatusNotFound, "no user with given id exists")
-			return
-		}
-		user := users[0]
-		c.JSON(http.StatusOK, user)
-	})
-
-	router.PATCH("/users/:userid", func(c *gin.Context) {
-		userid, err := urlParamToInt64(c.Param("userid"))
-		if err != nil {
-			l.Warn().Err(err).Msg("invalid payload for updating user")
-			c.String(http.StatusUnprocessableEntity, "invalid user format")
-			return
-		}
-
-		users, err := userService.ReadUserByUserID(userid)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to retrieve user from database")
-			c.String(http.StatusInternalServerError, "failed to retrieve user")
-			return
-		}
-
-		if len(users) == 0 {
-			c.String(http.StatusNotFound, "no user with given id exists")
-			return
-		}
-
-		user := users[0]
-		user.ID = userid
-		err = c.BindJSON(&user)
-		if err != nil {
-			l.Error().Err(err).Msg("user returned from database does not match expected schema")
-			c.String(http.StatusInternalServerError, "failed to retrieve user")
-			return
-		}
-		updated, err := userService.UpdateUser(user)
-		if err != nil {
-			l.Warn().Err(err).Int64("userid", user.ID).Msg("failed to update user in database")
-			c.String(http.StatusInternalServerError, "failed to update user")
-			return
-		}
-		c.JSON(http.StatusOK, updated)
-	})
-
-	router.DELETE("/users/:userid", func(c *gin.Context) {
-		userid, err := urlParamToInt64(c.Param("userid"))
-		if err != nil {
-			l.Warn().Err(err).Msg("invalid payload for deleting user")
-			c.String(http.StatusUnprocessableEntity, "invalid user format")
-			return
-		}
-		users, err := userService.ReadUserByUserID(userid)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to retrieve user from database")
-			c.String(http.StatusInternalServerError, "failed to retrieve user")
-			return
-		}
-		if len(users) == 0 {
-			c.String(http.StatusNotFound, "no user with given id exists")
-			return
-		}
-		user := users[0]
-		err = userService.DeleteUser(user)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to delete user from database")
-			c.String(http.StatusInternalServerError, "failed to delete user")
-			return
-		}
-		c.Status(http.StatusNoContent)
-	})
-
-	router.GET("/users/:userid/servers", func(c *gin.Context) {
-		userid, err := urlParamToInt64(c.Param("userid"))
-		if err != nil {
-			l.Warn().Err(err).Msg("invalid payload for user")
-			c.String(http.StatusUnprocessableEntity, "invalid user format")
-			return
-		}
-		servers, err := serverService.ReadServerByUserID(userid)
-		if err != nil {
-			l.Warn().Err(err).Msg("failed to fetch servers for given user")
-			c.String(http.StatusInternalServerError, "failed to delete user")
-			return
-		}
-		c.JSON(http.StatusOK, servers)
-	})
+	api.RegisterRoutes(router, handler)
 
 	router.GET("/servers", func(c *gin.Context) {
 		servers, err := serverService.ReadAllServers()
@@ -368,8 +250,6 @@ func main() {
 
 	// teapot
 	router.GET("/teapot", func(c *gin.Context) { c.Status(http.StatusTeapot) })
-
-	router.GET("/healthcheck", func(ctx *gin.Context) { ctx.Status(http.StatusOK) })
 
 	_ = router.Run("0.0.0.0:10000")
 
