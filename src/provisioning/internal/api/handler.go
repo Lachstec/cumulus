551ee6bee97f039eb8da5da503c7fbdb5f6ae6f7
Leon Lux <leonlux@icloud.com>
2025-02-15 19:24:36 +0100

    add handler for easily returning API responses

---
+++ src/provisioning/internal/api/handler.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"github.com/Lachstec/mc-hosting/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	UserService       services.UserService
+	ServerService     services.ServerService
+	FloatingIPService services.FloatingIPService
+}
+
+func (h *Handler) respondSuccess(c *gin.Context, code int, data interface{}) {
+	c.JSON(code, Response{
+		Status: "success",
+		Data:   data,
+	})
+}
+
+func (h *Handler) respondError(c *gin.Context, code int, message string, details interface{}) {
+	c.JSON(code, Response{
+		Status: "error",
+		Error: &Error{
+			Message: message,
+			Details: details,
+		},
+	})
+}
